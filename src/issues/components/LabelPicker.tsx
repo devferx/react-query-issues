fa2f67de864f1ad4d47a397a2a62a85cf2077925
devferx <ferkki@hotmail.com>
2022-11-04 23:18:02 -0400

    Seleccionar etiquetas

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,25 +1,36 @@
 import { LoadingIcon } from "../../shared/components/LoadingIcon";
 import { useLabels } from "../hooks/useLabels";
 
-export const LabelPicker = () => {
+interface LabelPickerProps {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker: React.FC<LabelPickerProps> = ({
+  selectedLabels,
+  onChange,
+}) => {
   const labelsQuery = useLabels();
 
   if (labelsQuery.isLoading) return <LoadingIcon />;
 
   return (
-    <div>
+    <>
       {labelsQuery.data?.map((label) => (
         <span
           key={label.id}
-          className="badge rounded-pill m-1 label-picker"
+          className={`badge rounded-pill m-1 label-picker ${
+            selectedLabels.includes(label.name) ? "label-active" : ""
+          }`}
           style={{
             border: `1px solid #${label.color}`,
             color: `#${label.color}`,
           }}
+          onClick={() => onChange(label.name)}
         >
           {label.name}
         </span>
       ))}
-    </div>
+    </>
   );
 };
--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import { IssueList } from "../components/IssueList";
 import { LabelPicker } from "../components/LabelPicker";
 
 export const ListView = () => {
+  const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
+
+  const onLabelChanged = (labelName: string) => {
+    selectedLabels.includes(labelName)
+      ? setSelectedLabels(selectedLabels.filter((label) => label !== labelName))
+      : setSelectedLabels([...selectedLabels, labelName]);
+  };
   return (
     <div className="row mt-5">
       <div className="col-8">
@@ -9,7 +17,10 @@ export const ListView = () => {
       </div>
 
       <div className="col-4">
-        <LabelPicker />
+        <LabelPicker
+          selectedLabels={selectedLabels}
+          onChange={(labelName) => onLabelChanged(labelName)}
+        />
       </div>
     </div>
   );
