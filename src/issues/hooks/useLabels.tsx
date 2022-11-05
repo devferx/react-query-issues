561dd267be1b198756e88d638ae129251ccb651d
devferx <ferkki@hotmail.com>
2022-11-04 22:11:54 -0400

    initialData y placeholderData

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -14,6 +14,27 @@ export const useLabels = () => {
   const labelsQuery = useQuery(["labels"], getLabels, {
     // refetchOnWindowFocus: false,
     staleTime: 1000 * 60 * 60,
+    // initialData: [],
+    placeholderData: [
+      {
+        id: 791921801,
+        node_id: "MDU6TGFiZWw3OTE5MjE4MDE=",
+        url: "https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F",
+        name: "❤️",
+        color: "ffffff",
+        default: false,
+        description: "",
+      },
+      {
+        id: 725156255,
+        node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+        url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+        name: "good first issue (taken)",
+        color: "b60205",
+        default: false,
+        description: "",
+      },
+    ],
   });
   return labelsQuery;
 };
