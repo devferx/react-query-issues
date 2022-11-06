6d569d0fdb4d935d6fcef531949593e4d5f7e4ea
devferx <ferkki@hotmail.com>
2022-11-06 15:02:11 -0400

    Cargar Issues de GitHub

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import { IssueList } from "../components/IssueList";
 import { LabelPicker } from "../components/LabelPicker";
+import { useIssues } from "../hooks/useIssues";
 
 export const ListView = () => {
   const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
+  const { issuesQuery } = useIssues();
 
   const onLabelChanged = (labelName: string) => {
     selectedLabels.includes(labelName)
       ? setSelectedLabels(selectedLabels.filter((label) => label !== labelName))
       : setSelectedLabels([...selectedLabels, labelName]);
   };
+
   return (
     <div className="row mt-5">
       <div className="col-8">
