2f6e70b0631630522de995785c81a5e2df719360
devferx <ferkki@hotmail.com>
2022-11-08 23:14:41 -0400

    [ADD] prefetch of issue comments

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -3,7 +3,7 @@ import { useQueryClient } from "@tanstack/react-query";
 import { FiInfo, FiMessageSquare, FiCheckCircle } from "react-icons/fi";
 
 import { Issue, State } from "../interfaces";
-import { getIssueInfo } from "../hooks/useIssue";
+import { getIssueInfo, getIssueComments } from "../hooks/useIssue";
 
 interface IssueItemProps {
   issue: Issue;
@@ -16,6 +16,10 @@ export const IssueItem = ({ issue }: IssueItemProps) => {
   const onMouseEnter = () => {
     queryClient.prefetchQuery(["issue", issue.number], () =>
       getIssueInfo(issue.number)
+    );
+
+    queryClient.prefetchQuery(["issue", issue.number, "comments"], () =>
+      getIssueComments(issue.number)
     );
   };
 
