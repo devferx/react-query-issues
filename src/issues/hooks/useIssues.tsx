791c5812a8f5805c3d05a81af6d9abb3ab9985d7
devferx <ferkki@hotmail.com>
2022-11-09 17:43:24 -0400

    Filtrar issues - Labels

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -15,6 +15,14 @@ const getIssues = async (labels: string[], state?: State): Promise<Issue[]> => {
 
   if (state) params.append("state", state);
 
+  if (labels.length > 0) {
+    const labelString = labels.join(",");
+    params.append("labels", labelString);
+  }
+
+  params.append("page", "1");
+  params.append("per_page", "5");
+
   const { data } = await githubApi.get<Issue[]>("/issues", { params });
   console.log(data);
   return data;
